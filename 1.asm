fa330dc8ea91d740868115a63dc0141fbe7540bd
swx522 <754607558@qq.com>
2025-11-02 17:56:58 +0800

    Add files via upload

---
+++ 1.asm
@@ -0,0 +1,19 @@
+data segment
+    msg db 'Hello,world!$'
+data ends
+
+code segment
+assume cs:code,ds:data
+start:
+    mov ax,data
+    mov ds,ax
+
+    mov ah,09h
+    mov dx,offset msg
+    int 21h
+
+    mov ah,4ch
+    int 21h
+
+code ends
+end start
